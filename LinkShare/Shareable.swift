1077051d4d6873e57dda39744f4f7af36ee4ece2
juhaelee <juhaelee@users.noreply.github.com>
2016-11-09 18:40:11 -0500

    shareable protocol, comment type, link type (#2)

---
+++ LinkShare/Shareable.swift
@@ -0,0 +1,18 @@
+//
+//  ShareableProtocol.swift
+//  LinkShare
+//
+//  Created by Ju Hae Lee on 11/9/16.
+//  Copyright © 2016 codepath-ajm. All rights reserved.
+//
+
+import Foundation
+
+protocol Shareable {
+    var id: String { get set }
+    var authorID: String { get set }
+    var comments: [Comment]? { get set }
+    var userIDs: [String] { get set }
+    var modifiedDate: Date { get set }
+    var lastReadDate: Dictionary<String, Date>? { get set }
+}
